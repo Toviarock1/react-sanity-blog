aa7d210dad9294a3f026afbe383d2d3a35e4f250
simon adama <simontoviaadama@gmail.com>
2021-11-02 21:08:15 +0100

    add advertisement section to our studio

---
+++ testblog/schemas/advertisement.js
@@ -0,0 +1,33 @@
+export default {
+    name: 'advertisement',
+    title: 'Advertisement',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'publishedAt',
+        title: 'Published at',
+        type: 'datetime',
+      },
+    ],
+  
+    preview: {
+      select: {
+        title: 'title',
+        media: 'mainImage',
+      }
+    },
+  }
+  
